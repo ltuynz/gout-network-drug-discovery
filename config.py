29217254ea2b9c8c32aa1b049a72b7896cbdfcf9
ltuynz <uyentoleuyen205@gmail.com>
2026-06-07 11:42:52 +0700

    Add configuration parameters for data paths and setting

---
+++ config.py
@@ -0,0 +1,10 @@
+DATA_DIR = "data/"
+
+HERB_PATH = DATA_DIR + "herb_target.csv"
+GOUT_TARGET_PATH = DATA_DIR + "gene_cards.csv"
+PPI_PATH = DATA_DIR + "interactome.txt"
+SYMPTOM_PATH = DATA_DIR + "symptom_gene.txt"
+
+RANDOM_STATE = 42
+N_RANDOM = 1000
+MIN_SYMPTOM_PROTEINS = 10
